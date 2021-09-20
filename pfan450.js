66addd0fbf832d6ee85bca0f4d61d330baaa2545
kavin1022 <pinghangfan1022@gmail.com>
2021-09-20 23:01:04 +1200

    added institute shop section, all major logical componments completed

--- pfan450.js
+++ pfan450.js
@@ -1,10 +1,16 @@
-const hideDiv =(divID) => {
-	let x = document.getElementById(divID.id);
+const hideDiv = (divID) => {
+	let x;
 
+	if (typeof divID === 'string' || divID instanceof String){
+		x = document.getElementById(divID);
+	}else{
+		x = document.getElementById(divID.id);
+	}
+	
 	//hide all block except for button block
-	const divList = ["homeWrapper", "staffWrapper", "instituteShopWrapper", "userRegWrapper", "guestBookWrapper"];
+	const divList = ["homeWrapper", "staffWrapper", "instituteShopWrapper", "userRegWrapper", "guestBookWrapper", "loginSectionWrapper"];
 	for (let i = 0; i < divList.length; i++){
-		if (divList[i] == divID.id){
+		if (divList[i] == divID.id || divList[i] == x){
 			continue;
 		}
 		let y = document.getElementById(divList[i]);
@@ -14,28 +20,123 @@ const hideDiv =(divID) => {
 	x.style.display = "block";
 }
 
-async function sendComment() {
-	const name = document.getElementById("commentName").value;
-	const body = document.getElementById("commentBody").value;
+const storeUserDetail = (username, password) => {
+	globalUsername = username;
+	globalPassword = password;
+	loginStatus = true;
+	document.getElementById("loginDisplay").innerHTML = username;
+}
 
-	if (name == "" || body == ""){
-		alert("Comment name and body cannot be empty!");
+const triggerLogin = () => {
+	hideDiv("loginSectionWrapper");
+}
+
+const loginAccount = async () => {
+	const username = document.getElementById("loginUsername").value;
+	const password = document.getElementById("loginPassword").value;
+	const data = {"username": username, "password": password};
+
+	if (username == "" || password == ""){
+		document.getElementById("loginMessage").innerHTML = "why you tryin to log in without a name and password";
 		return;
 	}
 
-	const data = {"comment": name, "name": body};
-	fetch("http://localhost:5000/api/WriteComment", {
-		headers: {"Content-Type": "application/json"},
+	fetch("http://localhost:5000/api/GetVersionA", {
+		headers: new Headers({
+			"Content-Type": "application/x-www-form-urlencoded",
+			"Authorization": "Basic " + btoa(username + ":" + password)
+		}),
+		method: "GET",
+	})
+	.then(response => {
+		if (response.status == 401){
+			document.getElementById("loginMessage").innerHTML = "Your password or username is wrong my bro";
+			
+		}
+		if (response.status == 200){
+			document.getElementById("loginMessage").innerHTML = "Loged in, now you can go spend some money in the shop";
+			storeUserDetail(username, password);
+			alert("Login successfully, please navigate to institute shop to purchase item");
+			hideDiv("homeWrapper");
+		}
+	});
+}
+const purchaseItem = (id) => {
+	if (loginStatus == false){
+		hideDiv("loginSectionWrapper");
+		alert("Please Login First");
+		return;
+	}
+
+	const data = {"productID": id, quantity: 1};
+	fetch("http://localhost:5000/api/PurchaseItem", {
+		headers: new Headers({
+			"Content-Type": "application/json",
+			"Authorization": "Basic " + btoa(globalUsername + ":" + globalPassword),	
+		}),
 		method: "POST",
 		body: JSON.stringify(data)
-	}).then(res => {
-		alert("Comment Posted!");
-		document.getElementById("com").src = document.getElementById("com").src;
 	})
+	.then(response => {
+		if(response.status == 201){
+			alert("Item Purchased Successfully");
+		}
+	});
+}
+//Shop
+const searchItems = async(e) => {
+	const parent = document.getElementById("shopItemsWrapper")
+	parent.innerHTML = null;
 
+	const updateItemList = (data) => {
+
+		let div = document.createElement("div");
+		let textdiv = document.createElement("div");
+		let itemTitle = document.createElement("h3");
+		let description = document.createElement("p");
+		let price = document.createElement("p");
+		let img = document.createElement("img");
+
+		itemTitle.appendChild(document.createTextNode(data.name));
+		description.appendChild(document.createTextNode(data.description));
+		price.appendChild(document.createTextNode("$" + data.price));
+		img.src = "http://localhost:5000/api/GetItemPhoto/" + data.id;
+
+		textdiv.appendChild(itemTitle);
+		textdiv.appendChild(description);
+		textdiv.appendChild(price);
+
+		let btn = document.createElement("button");
+		btn.innerHTML = "Purchase Item";
+		btn.onclick = () => {purchaseItem(data.id)}
+		textdiv.appendChild(btn)
+
+		div.appendChild(img);
+		div.appendChild(textdiv);
+		parent.appendChild(div);
+
+		img.className = "itemPhoto";
+		div.className = "shopItems";
+
+	};
+
+
+
+	const searchItemInput = document.getElementById("itemSearchBar");
+	fetch("http://localhost:5000/api/GetItems/" + searchItemInput.value, {
+		headers: {"Content-Type": "application/json"},
+		method: "GET",
+	})			
+	.then(response => response.json())
+	.then(data => {
+		for (let i = 0; i < data.length; i++){
+			updateItemList(data[i]);
+		}
+	});
 }
 
-async function getDisplayedStaff() {
+//Staff
+const getDisplayedStaff = async() => {
 
 	const createStaff = (parent, vccc) => {
 
@@ -102,8 +203,62 @@ async function getDisplayedStaff() {
 		.then(response => response.json())
 		.then(data => {
 			const parent = document.getElementById("staffList");
-			console.log(data);
 			fetchCards(parent, data);
 		});
 }
-//https://cws.auckland.ac.nz/335a3/api/GetCard/${data[i].id} http://localhost:5000/api/GetCard/7247480
+
+//User Registration
+const registerAccount = async() => {
+	const username = document.getElementById("username").value;
+	const password = document.getElementById("password").value;
+	const address = document.getElementById("address").value;
+
+	const data = {"username": username, "password": password, address: address};
+	fetch("http://localhost:5000/api/Register", {
+		headers: {"Content-Type": "application/json"},
+		method: "POST",
+		body: JSON.stringify(data)
+	})
+	.then(response => response.text())
+	.then(data => {
+		document.getElementById("resigerStatus").innerHTML = data;
+	});
+}
+
+//Guest Book
+const sendComment = async() => {
+	const name = document.getElementById("commentName").value;
+	const body = document.getElementById("commentBody").value;
+
+	if (name == "" || body == ""){
+		alert("Comment name and body cannot be empty!");
+		return;
+	}
+
+	const data = {"comment": body, "name": name};
+	
+	fetch("http://localhost:5000/api/WriteComment", {
+		headers: {"Content-Type": "application/json"},
+		method: "POST",
+		body: JSON.stringify(data)
+	}).then(res => {
+		alert("Comment Posted!");
+		document.getElementById("com").src = document.getElementById("com").src;
+	})
+}
+
+
+let loginStatus = false;
+let globalUsername, globalPassword;
+
+
+//enter key triggers login function
+let input = document.getElementById("loginPassword");
+input.addEventListener("keyup", (e) => {
+	if (e.key === "Enter"){
+		e.preventDefault();
+		loginAccount();
+	}
+})
+
+//search function listener
